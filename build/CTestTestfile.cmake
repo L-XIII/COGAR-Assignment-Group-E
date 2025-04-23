7cfc1d915e9b9a46efc5b32581fb9ecd95359dc2
Mamo1031 <mamoru_ota1031@keio.jp>
2025-04-23 17:10:50 +0000

    refactor: reorganize ROS package directory structure

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/blp/Documents/Projet/src
-# Build directory: /home/blp/Documents/Projet/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("group_E")
--- build/group_E/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/blp/Documents/Projet/src/group_E
-# Build directory: /home/blp/Documents/Projet/build/group_E
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
