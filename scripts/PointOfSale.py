0d253d6663ae41fe9122d52e854af02db4395406
Mamo1031 <mamoru_ota1031@keio.jp>
2025-04-24 10:48:46 +0000

    fix: Fixed order message concatenation method and loop condition error in PointOfSale.py

--- scripts/PointOfSale.py
+++ scripts/PointOfSale.py
@@ -31,14 +31,14 @@ class POS():
             - Priority (0 for no priority, 1 for priority)
         
         Example : "Table : 42, dish : Gunkan, loop n°1214, priority : 1"
-        
+
         """
         emission_possible = random.randint(1,200)
         if emission_possible<=3:
             dish = random.choice(POS.list_dishes) #Choose a random dish
             table_number = random.randint(1,60) #Choose a random table
             priority = random.randint(0,1) #Choose a random priority
-            order_msg = "Table : " + str(table_number) + ", dish : ", dish + ", loop n°" + str(nb_loops) + ", priority : " + priority
+            order_msg = "Table : " + str(table_number) + ", dish : " + dish + ", loop n°" + str(nb_loops) + ", priority : " + str(priority)
             rospy.loginfo("Message sent by POS : " + order_msg)
             self.publisher.publish(order_msg)
         self.rate.sleep()
@@ -52,7 +52,7 @@ class POS():
 if __name__ == '__main__':
     pos = POS()
     nb_loops = 0
-    while nb_loops>2000:#2 000 turns of loop, with a 1 Hz frequency
+    while nb_loops<2000:# 2000 turns of loop, with a 1 Hz frequency
         pos.order_emission(nb_loops)
         nb_loops+=1
     pos.shutdown()
