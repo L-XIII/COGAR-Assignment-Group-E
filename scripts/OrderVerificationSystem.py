a6addd84f3c4ab6a4b7fe0797c79c9afcd34880c
Mohamed El shab <112202370+L-XIII@users.noreply.github.com>
2025-04-24 17:56:56 +0200

    Update OrderVerificationSystem.py to align more with its specifc tasks

--- scripts/OrderVerificationSystem.py
+++ scripts/OrderVerificationSystem.py
@@ -16,30 +16,22 @@ class OrderVerificationSystem:
         self.dish = None
         self.speech_interface = SpeechInterface()
     
-    def verify_grasping(self):
+    def verify_served_order(self):
         """
-        - Verify that the targeted plate has been correctly grasped using the perception system of the TIAGo platform.
-        - Return "grasping error" if the perception system detects an error and "None" if not 
+        - Verify that the served plate corresponds correctly with the data recieved using the perception system of the TIAGo platform.
+        - Return "served order error" if the perception system detects an error and "None" if not 
         """
-        success = self.tiago.perception_system("grasping")
+        success = self.tiago.perception_system("Verified")
         if not success:
-            return "grasping error"
+            return "served order error"
         return None
 
-    def verify_placement(self):
-        """
-        - Verify that the plate has been placed correctly using the perception system of the TIAGo platform
-        - Return "placement error" if the perception system detects an error and "None" if not 
-        """
-        success = self.tiago.perception_system("placement")
-        if not success:
-            return "placement error"
-        return None
+   
     
     def verify_delivery_client(self):
         """
         - Verify with the client that the dish has been correctly served and there is no problem
-        - Returns "None" if there is no problem and the nature of the problem if there is one
+        - Returns "None" if there is no problem and the nature of the problem if there is one and notify the manager.
         """
         problem = self.speech_interface.verify_delivery_client()
         if problem == None:
